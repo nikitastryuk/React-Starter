87cb0bc8771c08ac47f2b6ef2ece68bc42a94d6d
Nikita Striuk <nikita.striuk@datarobot.com>
2020-01-22 21:19:56 +0200

    minimal react/babel/webpack setup

---
+++ webpack/webpack.common.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  output: {
+    path: path.resolve(__dirname, '../', 'build'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+};
---
+++ webpack/webpack.dev.js
@@ -0,0 +1,13 @@
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'eval-source-map',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
+};
