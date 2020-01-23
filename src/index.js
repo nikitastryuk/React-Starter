0edfbffa2dd08d946b8d6ea1648cc5b9b64c803f
Nikita Striuk <nikita.striuk@datarobot.com>
2020-01-23 12:13:46 +0200

    husky/lint-staged

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Minutes } from './Minutes';
 
+const p;
+
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(<Minutes />, rootElement);
