71d533e1556b03aecb898ec4203d5dae526eb46f
Nikita Striuk <nikita.striuk@datarobot.com>
2020-01-23 12:25:46 +0200

    lint-staged tested

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Minutes } from './Minutes';
 
-console.log('test');
-
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(<Minutes />, rootElement);
