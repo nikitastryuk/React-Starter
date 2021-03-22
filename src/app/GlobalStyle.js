2cbc1af24ec758e0e3211b1fa3a365c369ec4113
Nikita Striuk <nikita.striuk@datarobot.com>
2021-03-22 13:01:53 +0200

    back to fonts fetching

--- src/app/GlobalStyle.js
+++ src/app/GlobalStyle.js
@@ -1,15 +1,8 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
-// TODO: Good way to use fonts?
-import Roboto from '../assets/fonts/Roboto-Regular.ttf';
 
 export const GlobalStyle = createGlobalStyle`
   ${reset};
-
-  @font-face {
-    font-family: 'Roboto';
-    src: url(${Roboto}) format('truetype');
-  }
   body {
     overflow: hidden;
     font-family: 'Roboto', sans-serif;
