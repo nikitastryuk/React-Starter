750c84ad835e2ef20b6d36dee0d2fea77a25def3
Nikita Striuk <nikita.striuk@datarobot.com>
2021-03-31 01:30:30 +0300

    revert main page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -37,24 +37,23 @@ class TestApi {
 }
 
 export default function Main() {
-  return 123;
-  // const { t } = useTranslation();
-  // const { data: joke, isFetching } = useQuery('testData', getTestData, {
-  //   staleTime: 2500,
-  //   // Want errors to be thrown in the render phase and propagate to the nearest error boundary
-  //   useErrorBoundary: true,
-  // });
+  const { t } = useTranslation();
+  const { data: joke, isFetching } = useQuery('testData', getTestData, {
+    staleTime: 2500,
+    // Want errors to be thrown in the render phase and propagate to the nearest error boundary
+    useErrorBoundary: true,
+  });
 
-  // if (isFetching) {
-  //   return <StyledPage>{t('global.loading')}</StyledPage>;
-  // }
+  if (isFetching) {
+    return <StyledPage>{t('global.loading')}</StyledPage>;
+  }
 
-  // return (
-  //   <StyledPage fontsize="50px" data-testid="main-page">
-  //     <h1 data-testid="main-page-joke">{joke}</h1>
-  //     {/* <h2>{t('main.items', { postProcess: 'interval', count: 0 })}</h2> */}
-  //   </StyledPage>
-  // );
+  return (
+    <StyledPage fontsize="50px" data-testid="main-page">
+      <h1 data-testid="main-page-joke">{joke}</h1>
+      {/* <h2>{t('main.items', { postProcess: 'interval', count: 0 })}</h2> */}
+    </StyledPage>
+  );
 
   async function getTestData() {
     const { data } = await TestApi.getTestData();
