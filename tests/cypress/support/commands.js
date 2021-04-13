ff679627f80d822da803ab00a599c16d0c71122c
Nikita Striuk <nikita.striuk@datarobot.com>
2021-04-13 22:38:19 +0300

    cypress folder moved

--- tests/cypress/support/commands.js
+++ tests/cypress/support/commands.js
@@ -3,5 +3,5 @@ const { ROUTE_PATHS } = require('routes');
 Cypress.Commands.add('login', () => {
   cy.visit(ROUTE_PATHS.LOGIN);
   cy.get('button[type=submit]').click();
-  cy.get('[data-testid="main-page"]').should('be.visible');
+  cy.get('[data-testid="main-page"]', { timeout: 5000 }).should('be.visible');
 });
